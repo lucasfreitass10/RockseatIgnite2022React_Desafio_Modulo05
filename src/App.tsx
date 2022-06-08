985a3e0d2ff1177e56ae6ea34b0a35da6e464998
Lucas Freitas <lucasfreitass10@live.com>
2022-06-08 20:54:02 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { lazy, Suspense } from 'react';
+import { Loading } from './components/Loading';
+import { SideBar } from './components/SideBar';
+import { MoviesContextProvider } from './context/MoviesContext';
+import './styles/global.scss';
+
+const Content = lazy(() => import('./components/Content'));
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function App() {
+  return (
+    <MoviesContextProvider>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <SideBar />
+
+        <Suspense fallback={<Loading />}>
+          <Content />
+        </Suspense>
+      </div>
+    </MoviesContextProvider>
+  );
+}
